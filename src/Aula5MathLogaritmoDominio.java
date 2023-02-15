4c8ee82dc9e495a7d964a97d9348e647ebe47821
Marieli Ficagna <marieli.ficagna@gmail.com>
2023-02-15 16:25:40 -0300

    first commit

---
+++ src/Aula5MathLogaritmoDominio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Mari (48) 99618-5728
+ */
+public class Aula5MathLogaritmoDominio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float nr = 0.1f;
+        System.out.println("Resultado 1: " + Math.log(nr+1));
+        System.out.println("Resultado 2: " + Math.log1p(nr));
+
+    }
+    
+}
